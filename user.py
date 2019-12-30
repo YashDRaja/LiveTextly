fa467b823f36fe35747cfcc8d0e4b67d99823d5b
YashDRaja <56655681+YashDRaja@users.noreply.github.com>
2019-12-30 17:55:20 -0500

    Class User
    The class user is used to store data about a client

--- user.py
+++ user.py
@@ -1,6 +1,7 @@
 class User:
     def __init__(self, id):
         self.id = id
+        self.password = None
         self.receiver = None
         self.received = (None,None)
         self.history = []
